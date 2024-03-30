687d27ba38024f94a4ee443c3a68b8d4261313c3
jorge <jbousing@gmail.com>
2024-03-29 21:09:40 -0400

    sala de chat

--- src/index.js
+++ src/index.js
@@ -17,7 +17,35 @@ app.get("/", (req, res) => {
 
 io.on("connection", (socket) => {
 
-  
+  socket.leave(socket.connectedRoom);
+
+  socket.on("connect to room", (room) => {
+    switch(room) {
+      case "room1":
+        socket.join("room1");
+        socket.connectedRoom = "room1";
+        break;
+      case "room2":
+        socket.join("room2");
+        socket.connectedRoom = "room2";
+        break;
+      case "room3":
+        socket.join("room3");
+        socket.connectedRoom = "room3";
+        break;    
+    }
+  });
+
+  socket.on("message", (message) => {
+
+    const room = socket.connectedRoom;
+
+    io.to(room).emit("send message", {
+      message,
+      room
+    });
+
+  });
 });
 
 
--- src/views/js/index.js
+++ src/views/js/index.js
@@ -1,4 +1,42 @@
-
-
 const socket = io();
 
+//SELECCION DE BOTONES QUE PERMITIRAN CONECTAR A LA SALA
+const connectRoom1 = document.querySelector("#connectRoom1");
+const connectRoom2 = document.querySelector("#connectRoom2");
+const connectRoom3 = document.querySelector("#connectRoom3");
+
+
+//EVENTOS QUE AL HACER CLICK ME CONECTE A LAS SALAS
+connectRoom1.addEventListener("click", () => {
+    socket.emit("connect to room","room1");
+});
+
+connectRoom2.addEventListener("click", () => {
+    socket.emit("connect to room","room2");
+});
+
+connectRoom3.addEventListener("click", () => {
+    socket.emit("connect to room","room3");
+});
+
+//ENVIAR MENSAJES
+const sendMessage = document.querySelector("#sendMessage");
+
+sendMessage.addEventListener("click", () => {
+
+    console.log("sdasdsdadadkadkd")
+    const message = prompt("Escribe tu mensaje: ");
+
+    socket.emit("message", message);
+});
+
+//RECIBIR EL EVENTO DEL MENSAJE
+socket.on("send message", (data) => {
+
+    const {room, message } = data;
+
+    const li = document.createElement("li");
+    li.textContent = message;
+
+    document.querySelector(`#${room}`).append(li);
+});
